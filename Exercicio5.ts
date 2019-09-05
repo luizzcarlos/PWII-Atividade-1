beecb6cd704775d11b01b755f902bece23178363
luizzcarlos <juninho_2014br@hotmail.com>
2019-09-04 22:14:11 -0300

    Exercicio 5 Revisado

---
+++ Exercicio5.ts
@@ -0,0 +1,8 @@
+let largura: number = 250
+let comprimento: number = 500
+
+let Area = function (larg:number, comp:number) : number {
+    return (larg * comp)
+}
+
+console.log (`A área da figura é ${Area(largura,comprimento)}, com comprimento ${comprimento} e largura ${largura}.`)
---
+++ exercicio3-4.ts
@@ -0,0 +1,10 @@
+let numero1 :number =2
+let fParImpar1 = function (num:number) :number {
+    return num%2;
+}  
+    if (fParImpar1(numero) == 0) {
+        console.log (`Esse número é par: ${numero}`)
+    }
+    else {
+        console.log (`Esse número é ímpar: ${numero}`)
+    }
